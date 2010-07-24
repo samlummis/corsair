253fe3fe1b5158c9231affca344c41f6788f8ba5
nick <nick@nick-laptop.(none)>
2010-07-24 16:31:00 +1200

    Presentation version

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,6 +1,13 @@
 class PlannerController < ApplicationController
 
+
+
 def index
+  @course_list = session[:course_list] || []
+  if params[:course_id]
+    @course_list.push(Course.find(params[:course_id])) 
+    session[:course_list] = @course_list
+  end    
 	@courses = Course.find_courses
 end
 
