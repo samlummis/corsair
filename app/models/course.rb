e7b46a4570f6b396406d152198748d7c7a556099
Cameron Fowler <cameron@cameron-laptop.(none)>
2010-07-24 15:04:37 +1200

    fixed the index page for planner. now displays a list of courses and codes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,20 @@
 class Course < ActiveRecord::Base
 
-has_many :streams
+  has_many :streams
 
-def self.find_courses
-	find(:all, :order => "course")
-end
+  def self.find_courses
+    find(:all, :order => "course")
+  end
+
+  def code_string
+    case
+    when code < 10
+      "00#{code}"
+    when code < 100
+      "0#{code}"
+    else
+      code
+    end
+  end
 
 end
